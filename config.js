2c4a3e3204b34c7b5b1f86d77d2bed3cdca4abf3
pikachu625169 <mrhacker625169@gmail.com>
2025-07-31 22:23:14 +0600

    Update config.js

--- config.js
+++ config.js
@@ -3,16 +3,16 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  ownerID: ["1208088138338271272", ""], 
+  mongodbUri : "mongodb+srv://public:public6969@cluster0.hcetn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  activityName: "SAMI ON TOP", 
+  activityType: "Streaming",  // Available activity types : LISTENING , PLAYING
+  SupportServer: "https://discord.gg/wmDnS3VBQd",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
